bc54bbc4fc20cc15f116b2da58f1b3352fa0e175
glennangelico <mindslogic.mg@gmail.com>
2024-11-07 21:36:35 +0300

    task_2

---
+++ mcb-dynamics-365-angular/src/app/shared/config.ts
@@ -0,0 +1,4 @@
+export const client_secret = 'sak8Q~qCglP3uDAiqFag1ZLcLsO1M3y9Wjmtfb3X';
+export const tenant_id = '3f399a9d-bcb2-47f8-bd7e-2661728292ea';
+export const client_id = '43c7da09-0d40-40c7-91dc-6087b940e3ff';
+export const url = 'https://org7c730ca8.crm4.dynamics.com/';
